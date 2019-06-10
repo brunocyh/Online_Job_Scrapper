4a6dbb4cb1110d1a82034950683273d587492cdb
bruno <brunocyh@gmail.com>
2019-06-10 21:54:20 +1000

    developing skeleton

---
+++ lib/ContentManagement/Analyser.py
@@ -0,0 +1,10 @@
+class Crawler():
+    def __init__(self):
+        pass
+
+    def crawl_content(self):
+        pass
+
+    def export_csv(self):
+        pass
+
---
+++ main.py
@@ -0,0 +1,15 @@
+class SearchEngine():
+    def __init__(self):
+        pass
+
+    def scan_jobboards(self):
+        pass
+
+    def get_recent_results(self):
+        pass
+
+    def retrieve_results(self):
+        pass
+
+if __name__ == "__main__":
+    pass
