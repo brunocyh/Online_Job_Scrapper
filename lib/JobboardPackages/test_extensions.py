38f580e92cf259c8384dbcb68df6f4f612b72bc4
bruno <brunocyh@gmail.com>
2019-06-18 16:01:14 +1000

    text different jboard

--- lib/JobboardPackages/test_extensions.py
+++ lib/JobboardPackages/test_extensions.py
@@ -1,6 +1,10 @@
-from lib.JobboardPackages.Indeed import IndeedSerach
 from lib.CrawlerService.Crawler import Crawler
 from lib.DatabaseService.JobDBService import JobDatabase
+
+from lib.JobboardPackages.Indeed import IndeedSerach
+from lib.JobboardPackages.Seek import SeekSerach
+from lib.JobboardPackages.Neuvoo import NeuvooSerach
+from lib.JobboardPackages.Gumtree import GumtreeSerach
 
 
 def test_integration_indeed():
@@ -14,3 +18,42 @@ def test_integration_indeed():
     database.commit()
     database.disconnect()
     assert response == True
+
+
+def test_integration_seek():
+    crawler = Crawler()
+
+    database = JobDatabase.instantiate_Database()
+    database.connect()
+    searcher = SeekSerach(crawler, database)
+    response = searcher.search_board(term='barista', location='brisbane')
+
+    database.commit()
+    database.disconnect()
+    assert response == True
+
+
+# def test_integration_neuvoo():
+#     crawler = Crawler()
+
+#     database = JobDatabase.instantiate_Database()
+#     database.connect()
+#     searcher = NeuvooSerach(crawler, database)
+#     response = searcher.search_board(term='barista', location='brisbane')
+
+#     database.commit()
+#     database.disconnect()
+#     assert response == True
+
+
+# def test_integration_gumtree():
+#     crawler = Crawler()
+
+#     database = JobDatabase.instantiate_Database()
+#     database.connect()
+#     searcher = GumtreeSerach(crawler, database)
+#     response = searcher.search_board(term='barista', location='brisbane')
+
+#     database.commit()
+#     database.disconnect()
+#     assert response == True
